1ab817b0cdac6fdf388c8aecffd5ac5a96e0d357
Sayira Elittzi Quirama <sequirama@soy.sena.edu.co>
2024-04-09 18:43:30 -0500

    Initial commit

---
+++ src/test/java/com/example/bootcamp_2024_onclass_ms_user/Bootcamp2024OnclassMsUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bootcamp_2024_onclass_ms_user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bootcamp2024OnclassMsUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
